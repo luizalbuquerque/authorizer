60a8d6f03030992714466a9195dd0cb175cae4cd
Luiz de Albuquerque <luiz.info.2016@gmail.com>
2022-11-21 13:52:20 -0300

    create a dml file to data

--- src/main/java/com/card/authorizer/repository/TransacaoRepository.java
+++ src/main/java/com/card/authorizer/repository/TransacaoRepository.java
@@ -2,7 +2,11 @@ package com.card.authorizer.repository;
 
 import com.card.authorizer.entity.TransacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface TransacaoRepository extends JpaRepository<TransacaoEntity, Long> {
 
 }
--- src/main/java/com/card/authorizer/resource/TransacaoResource.java
+++ src/main/java/com/card/authorizer/resource/TransacaoResource.java
@@ -1,6 +1,11 @@
 package com.card.authorizer.resource;
 
 import com.card.authorizer.dto.TransacaoDTO;
+import com.card.authorizer.entity.CartaoEntity;
+import com.card.authorizer.entity.TransacaoEntity;
+import com.card.authorizer.exeption.ResourceNotFoundException;
+import com.card.authorizer.repository.CartaoRepository;
+import com.card.authorizer.repository.TransacaoRepository;
 import com.card.authorizer.service.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,9 +20,15 @@ public class TransacaoResource {
     @Autowired
     private TransacaoService transacaoService;
 
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<TransacaoDTO> insert(@RequestParam String numeroCartao, String senhaCartao,  String valorTransacao) {
         return null;
     }
+
 }
